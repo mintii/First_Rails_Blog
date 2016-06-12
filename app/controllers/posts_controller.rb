29fbd32669f1ef45beecd2a64461aa297e490e8a
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-11 20:32:01 -0500

    Complete Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,44 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def show
+    # As we already have our “before_action” filter set on the “show” action,
+    # it does the job for us to find into the Post model database the one with
+    # the “:id” provided in the URL params
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "The post was created!"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Update successful"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path, notice: "Post destroyed"
   end
 
 private
