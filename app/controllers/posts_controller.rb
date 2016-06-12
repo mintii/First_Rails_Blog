6bc7aba1298e59204e7d2e9afe68ba2961c4eb24
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-12 17:47:50 -0500

    Create protected section of Posts for authentication of admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:admin, :new, :create, :edit, :update, :destroy ]
 
   def index
     if params[:category].blank?
@@ -44,6 +45,17 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to root_path, notice: "Post destroyed"
+  end
+
+  def admin
+    redirect_to root_path if authenticate
+  end
+
+protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "test" && password == "test"
+    end
   end
 
 private
